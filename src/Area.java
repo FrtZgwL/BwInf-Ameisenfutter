71f1257ef5036a33236127209b4fcc515041da76
Cedric <cedrictd.ctd@gmail.com>
2015-09-25 15:04:17 +0200

    Implement positional data.

--- src/Area.java
+++ src/Area.java
@@ -3,18 +3,29 @@
  */
 public class Area {
 
+    private final int width;
+    private final int height;
+    private final Position nest;
+    private final Position[] foods;
     private Field[][] fields;
 
-    public Area(int width, int height, Position nest, Position[] food) {
-
+    public Area(int width, int height, Position nest, Position[] foods) {
+        this.width = width;
+        this.height = height;
+        this.nest = nest;
+        this.foods = foods;
     }
 
     /**
-     *
+     * Return the position of the nest
      * @return Position des Nestes
      */
     public Position getNestPos() {
         return null;
+    }
+
+    public Position[] getFoodPos() {
+        return foods;
     }
 
     /**
@@ -33,4 +44,11 @@ public class Area {
         // step Fields
     }
 
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 }
